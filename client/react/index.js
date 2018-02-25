2bd7ee0122ef6475c72f3622c70045a240f7747d
Owner <owner@MacBook-Pro.local>
2018-02-25 18:59:52 -0500

    fixing deployment issue

--- client/react/index.js
+++ client/react/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
-import App from './components/App';
+import App from './Components/App';
 import reducers from './reducers';
 
 const store = applyMiddleware(ReduxPromise)(createStore);
