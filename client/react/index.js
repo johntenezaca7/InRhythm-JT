80cabafbd87245288ed59d5ab8b0003cf39042bf
Owner <owner@MacBook-Pro.local>
2018-02-25 18:42:09 -0500

    deploying to heroku

--- client/react/index.js
+++ client/react/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
-import App from './components/app';
+import App from './components/App';
 import reducers from './reducers';
 
 const store = applyMiddleware(ReduxPromise)(createStore);
